902461764847e63be2d508c51948a8016dbd5681
edamame-maru <so1maru.ueda@gmail.com>
2025-03-07 22:03:10 +0000

    fixed symlinks

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 mkdir -p ~/.config/i3/
 mkdir -p ~/.config/i3status/
 
-ln -s ./i3_config_file ~/.config/i3/config
-ln -s ./i3status_config_file ~/.config/i3status/config
-ln -s ./git_config_file ~/.gitconfig
-ln -s ./xinit_config_file ~/.xinitrc
+ln -s ~/.dots/i3_config_file ~/.config/i3/config
+ln -s ~/.dots/i3status_config_file ~/.config/i3status/config
+ln -s ~/.dots/git_config_file ~/.gitconfig
+ln -s ~/.dots/xinit_config_file ~/.xinitrc
 
